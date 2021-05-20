1c1e322c0a6c089e803bc59d03894e713e84bdb1
Camilla <camilla.lima000@gmail.com>
2021-05-20 18:18:38 +0100

    Step 14 / Ex. 1 - 8

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+require "csv"
 @students = []
 
 def input_students
@@ -19,12 +20,22 @@ def input_students
     puts "What is the student's age?"
     age = STDIN.gets.chomp
 
-    @students << { name: name, cohort: cohort, nationality: nationality, age: age }
+    @students << student_hash([name, cohort, nationality, age])
 
     puts "Now we have #{@students.count} students"
     puts "Please enter another name, or press enter to skip"
     name = STDIN.gets.chomp
   end
+end
+
+def student_hash(array)
+  identity_hash = {
+    name: array[0],
+    cohort: array[1],
+    nationality: array[2],
+    age: array[3],
+  }
+  identity_hash
 end
 
 def print_header
@@ -67,8 +78,8 @@ def print_menu
   #1. Print the menu and ask user what to do
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a file"
+  puts "4. Load the list from the file"
   puts "9. Exit"
 end
 
@@ -76,10 +87,12 @@ def process(selection)
   case selection
   when "1"
     input_students
+    puts "You've added a student"
   when "2"
     show_students
   when "3"
     save_students
+    puts "You have saved students' details to the file."
   when "4"
     load_students
   when "9"
@@ -99,40 +112,43 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def save_students
-  file = File.open("students.csv", "w")
+  puts "Enter the name of the file to save: "
+  filename = STDIN.gets.chomp
+  CSV.open(filename, "w")
   @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+    student_data =
+      [student[:name], student[:cohort], student[:nationality], student[:age]]
+    file.puts student_data.join(",")
   end
   file.close
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+  puts "Enter the name of the file you want to open: "
+  filename = STDIN.gets.chomp
+  file = CSV.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    @students << { name: name, cohort: cohort.to_sym }
+    student = line.chomp.split(",")
+    @students << student_hash(student)
   end
   file.close
 end
 
 def try_load_students
-  filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
+  filename = "students.csv"
+  return if filename.nil?
+  if File.exists?(filename)
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
+    puts "Loaded #{students.count} from #{filename}"
+  else
     puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
+    exit
   end
 end
 
-try_load_students
 interactive_menu
